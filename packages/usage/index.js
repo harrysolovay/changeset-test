b5e4856285f2548c088fb75c80bc491d8e17a30a
Harry Solovay <harrysolovay@gmail.com>
2019-11-14 20:26:09 -0800

    first commit & changeset

---
+++ packages/usage/index.js
@@ -0,0 +1,5 @@
+const core = require("@neverusethis/core")
+const pluginA = require("@neverusethis/plugin-a")
+const pluginB = require("@neverusethis/plugin-b")
+
+console.log(core, pluginA, pluginB)
